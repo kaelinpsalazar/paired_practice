ec72069665fe5f83b168859880e6b75adb7fdce1
Kaelin Salazar <kaelinpsalazar@gmail.com>
2024-05-23 09:43:35 -0600

    added code

---
+++ practice.rb
@@ -0,0 +1,17 @@
+states = {
+           "Oregon" => "OR",
+           "Alabama" => "AL",
+           "New Jersey" => "NJ",
+           "Colorado" => "CO"
+         }
+
+capitals = {
+             "OR" => "Salem",
+             "AL" => "Montgomery",
+             "NJ" => "Trenton",
+             "CO" => "Denver"
+           }
+           def state_to_capital(state_name, states, capitals)
+            abbreviation = states[state_name]
+            abbreviation ? capitals[abbreviation] : "Unknown"
+          end
