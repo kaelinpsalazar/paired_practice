d3d3763376baa257bad16b673e7b1d77b0b15ce0
Kaelin Salazar <kaelinpsalazar@gmail.com>
2024-05-23 10:25:28 -0600

    added stuff

--- practice.rb
+++ practice.rb
@@ -15,3 +15,9 @@ capitals = {
             abbreviation = states[state_name]
             abbreviation ? capitals[abbreviation] : "Unknown"
           end
+
+          puts state_to_capital("Oregon", states, capitals) # Output: Salem
+          puts state_to_capital("California", states, capitals) # Output: Unknown
+          
+          puts capital_to_state("Trenton", states, capitals) # Output: New Jersey
+          puts capital_to_state("Boston", states, capitals) # Output: Unknown
